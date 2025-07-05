07d296b52a122d19ca16248baf49f9e9c0c3dcb1
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-07-06 01:25:11 +0600

    added video play gorund

--- src/app/student/[id]/page.tsx
+++ src/app/student/[id]/page.tsx
@@ -85,7 +85,7 @@ const LearningPlatform = () => {
                             className="w-full"
                             poster="/video-poster.jpg"
                         >
-                            <source src="/hero-video.mp4" type="video/mp4" />
+                            <source src="/videos/hero-video.mp4" type="video/mp4" />
                             Your browser does not support the video tag.
                         </video>
                     </div>
